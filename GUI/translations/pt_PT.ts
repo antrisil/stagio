530ae2c123b0f0f347aee01655bcb30034600196
antrisil <jmrr_ribeiro@hotmail.com>
2023-04-11 14:27:24 +0100

    commit1

---
+++ GUI/translations/pt_PT.ts
@@ -0,0 +1,184 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt_BR">
+<context>
+    <name>MONITOR</name>
+    <message>
+        <location filename="../MONITOR.qml" line="108"/>
+        <source>MENU</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../MONITOR.qml" line="127"/>
+        <source>START</source>
+        <translation>PARAR</translation>
+    </message>
+    <message>
+        <location filename="../MONITOR.qml" line="145"/>
+        <source>LOAD</source>
+        <translation>CARGA</translation>
+    </message>
+</context>
+<context>
+    <name>booting</name>
+    <message>
+        <location filename="../booting.qml" line="23"/>
+        <source>BOOTING</source>
+        <translation>CARREGANDO</translation>
+    </message>
+</context>
+<context>
+    <name>bypass</name>
+    <message>
+        <location filename="../bypass.qml" line="45"/>
+        <source>Exit</source>
+        <translation>Sair</translation>
+    </message>
+</context>
+<context>
+    <name>grafic</name>
+    <message>
+        <location filename="../grafic.qml" line="42"/>
+        <source>Load</source>
+        <translation>Carga</translation>
+    </message>
+    <message>
+        <location filename="../grafic.qml" line="406"/>
+        <source>Exit</source>
+        <translation>Sair</translation>
+    </message>
+</context>
+<context>
+    <name>languages</name>
+    <message>
+        <location filename="../languages.qml" line="20"/>
+        <source>LANGUAGE</source>
+        <translation>IDIOMAS</translation>
+    </message>
+    <message>
+        <location filename="../languages.qml" line="51"/>
+        <source>English</source>
+        <translation>Inglês</translation>
+    </message>
+    <message>
+        <location filename="../languages.qml" line="51"/>
+        <source>Portuguese</source>
+        <translation>Português</translation>
+    </message>
+    <message>
+        <location filename="../languages.qml" line="51"/>
+        <source>Exit</source>
+        <translation>Sair</translation>
+    </message>
+</context>
+<context>
+    <name>metric</name>
+    <message>
+        <location filename="../metric.qml" line="20"/>
+        <source>METRICS</source>
+        <translation>MÉTRICAS</translation>
+    </message>
+    <message>
+        <location filename="../metric.qml" line="54"/>
+        <source>Temperature : </source>
+        <translation>Temperatura : </translation>
+    </message>
+    <message>
+        <location filename="../metric.qml" line="54"/>
+        <source>Humidity : </source>
+        <translation>Humidade : </translation>
+    </message>
+    <message>
+        <location filename="../metric.qml" line="54"/>
+        <source>Exit</source>
+        <translation>Sair</translation>
+    </message>
+</context>
+<context>
+    <name>output</name>
+    <message>
+        <location filename="../output.qml" line="23"/>
+        <source>OUTPUT</source>
+        <translation>OUTPUT</translation>
+    </message>
+    <message>
+        <location filename="../output.qml" line="81"/>
+        <source>Exit</source>
+        <translation>Sair</translation>
+    </message>
+</context>
+<context>
+    <name>settings</name>
+    <message>
+        <location filename="../settings.qml" line="26"/>
+        <source>MENU</source>
+        <translation>MENU</translation>
+    </message>
+    <message>
+        <location filename="../settings.qml" line="69"/>
+        <source>Home</source>
+        <translation>Inicio</translation>
+    </message>
+    <message>
+        <location filename="../settings.qml" line="69"/>
+        <source>Languages</source>
+        <translation>Idiomas</translation>
+    </message>
+    <message>
+        <location filename="../settings.qml" line="69"/>
+        <source>Output</source>
+        <translation>Output</translation>
+    </message>
+    <message>
+        <location filename="../settings.qml" line="69"/>
+        <source>Metrics</source>
+        <translation>Métricas</translation>
+    </message>
+    <message>
+        <location filename="../settings.qml" line="69"/>
+        <source>Bypass</source>
+        <translation>Bypass</translation>
+    </message>
+    <message>
+        <location filename="../settings.qml" line="69"/>
+        <source>StandBy</source>
+        <translation>StandBy</translation>
+    </message>
+    <message>
+        <location filename="../settings.qml" line="69"/>
+        <source>Maintenance</source>
+        <translation>Manutenção</translation>
+    </message>
+</context>
+<context>
+    <name>standby</name>
+    <message>
+        <location filename="../standby.qml" line="98"/>
+        <source>Exit</source>
+        <translation>Sair</translation>
+    </message>
+    <message>
+        <location filename="../standby.qml" line="277"/>
+        <source>Timer : </source>
+        <translation>Temporizador : </translation>
+    </message>
+</context>
+<context>
+    <name>temperature</name>
+    <message>
+        <location filename="../temperature.qml" line="17"/>
+        <source>TEMPERATURE</source>
+        <translation>TEMPERATURA</translation>
+    </message>
+    <message>
+        <location filename="../temperature.qml" line="35"/>
+        <source>Temperature :</source>
+        <translation>Temperatura :</translation>
+    </message>
+    <message>
+        <location filename="../temperature.qml" line="60"/>
+        <source>Exit</source>
+        <translation>Sair</translation>
+    </message>
+</context>
+</TS>
